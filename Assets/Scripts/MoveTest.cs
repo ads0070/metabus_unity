d4372b15d33eabe2283cba73e8c019db1da2bead
ads0070 <gch05610@naver.com>
2022-01-03 16:16:17 +0900

    add SampleScene

--- Assets/Scripts/MoveTest.cs
+++ Assets/Scripts/MoveTest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveTest : MonoBehaviour
 {
-    Vector3 target = new Vector3(0, 5, 70);
+    Vector3 target = new Vector3(10, 0, 70);
 
     // Update is called once per frame
     void Update()
