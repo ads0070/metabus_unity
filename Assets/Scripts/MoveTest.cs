dcfdd8026d943ea394ef2f36ffdbf60cb980c5d3
ads0070 <gch05610@naver.com>
2022-01-03 16:06:50 +0900

    add models

---
+++ Assets/Scripts/MoveTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour
+{
+    Vector3 target = new Vector3(0, 5, 70);
+
+    // Update is called once per frame
+    void Update()
+    {
+        //1.MoveTowards
+        /*transform.position =
+            Vector3.MoveTowards(transform.position, target, 1f);*/
+
+        //2.SmoothDamp
+        Vector3 velo = Vector3.zero;
+
+        transform.position =
+            Vector3.SmoothDamp(transform.position, target, ref velo, 0.5f);
+
+    }
+}
