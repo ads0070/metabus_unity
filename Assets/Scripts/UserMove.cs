086b204e179e15f5a0f62dfdc1ecee0f7a7f8555
김기태 <rlarlxo2323@naver.com>
2022-01-11 20:11:11 +0900

    attach map in unity

--- Assets/Scripts/UserMove.cs
+++ Assets/Scripts/UserMove.cs
@@ -5,51 +5,54 @@ using System.Collections;
 public class UserMove : MonoBehaviour
 {
 
-	public float speed;
-	public float v;
-	public float h;
-	public bl_Joystick Joystick;//Joystick reference for assign in inspector
+    private float speed;
+    private float v;
+    private float h;
+    public bl_Joystick Joystick;
+    public Transform centralAxis;
 
-	Vector3 moveVec;
-	Animator anim;
+    Vector3 moveVec;
+    Animator anim;
 
-	void Awake()
-	{
-		anim = GetComponentInChildren<Animator>();
-	}
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
 
-	void Update()
-	{
-		v = Joystick.Vertical; //get the vertical value of joystick
-		h = Joystick.Horizontal; //get the horizontal value of joystick
+    void Update()
+    {
+        v = Joystick.Vertical;
+        h = Joystick.Horizontal;
 
-		moveVec = new Vector3(h, 0, v).normalized;
+        moveVec = new Vector3(h, 0, v).normalized;
 
-		transform.position += moveVec * speed * Time.deltaTime;
+        Debug.Log(centralAxis.transform.eulerAngles);
 
-		if (moveVec != Vector3.zero)
+        if (moveVec != Vector3.zero)
         {
-			if (System.Math.Abs(v) < 2.5 && System.Math.Abs(h) < 2.5)
+            if (System.Math.Abs(v) < 2.5 && System.Math.Abs(h) < 2.5)
             {
-				anim.SetBool("isRun", false);
-				anim.SetBool("isWalk", true);
-				speed = 15;
-			}
-			else
+                anim.SetBool("isRun", false);
+                anim.SetBool("isWalk", true);
+                speed = 15;
+            }
+            else
             {
-				anim.SetBool("isRun", true);
-				anim.SetBool("isWalk", false);
-				speed = 30;
-			}
+                anim.SetBool("isRun", true);
+                anim.SetBool("isWalk", false);
+                speed = 30;
+            }
         }
-		else
+        else
         {
-			anim.SetBool("isRun", false);
-			anim.SetBool("isWalk", false);
-		}
-		
+            anim.SetBool("isRun", false);
+            anim.SetBool("isWalk", false);
+        }
 
-		transform.LookAt(transform.position + moveVec);
+        transform.position += moveVec * speed * Time.deltaTime;
 
-	}
+        transform.LookAt(transform.position + moveVec);
+
+        centralAxis.position = transform.position;
+    }
 }
